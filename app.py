5ee05ec044945dbeff5758a025dae01551c2429d
worshix <worshiplivingstone@gmail.com>
2025-03-01 16:38:29 +0200

    flask structure init

--- app.py
+++ app.py
@@ -10,5 +10,21 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/reset')
+def reset():
+    return render_template('reset.html')
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
+@app.route('lecturer')
+def lecturer():
+    return render_template('lecturer.html')    
+
 if __name__ == '__main__':
     app.run(debug=True)
